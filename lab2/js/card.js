ce1c26adcd431bb1c7fbb65d7712f13928edee05
ZienabAlr <r0784494@student.thomasmore.be>
2022-10-11 21:38:05 +0200

    feat(card.js): winner is checked and saved to storage

--- lab2/js/card.js
+++ lab2/js/card.js
@@ -42,6 +42,9 @@ export default class Card {
       // call checkWinner() on the Bingo class
       // try to call the save() method on the Bingo class
        
+      this.markDone(e.target);
+      Bingo.checkWinner();
+      Bingo.save();
       //console.log("clicked");
     });
   }
